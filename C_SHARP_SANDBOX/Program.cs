bc17c4709d0fa48f3613931fd2caad1a564458ea
ZEER0H <calbers1@harding.edu>
2021-09-29 09:28:22 -0500

    Add project files.

---
+++ C_SHARP_SANDBOX/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_SHARP_SANDBOX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int i = 12345;
+                byte b = Convert.ToByte(i);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Type Cannot Be Converted!");
+            }
+
+
+
+        }
+    }
+}
